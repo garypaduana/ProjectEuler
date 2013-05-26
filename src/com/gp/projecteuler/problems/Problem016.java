e2ea24079a47b4a5cd2580ee5dc7224e12be38f6
Gary Paduana <gary.paduana@gmail.com>
2013-05-26 16:43:41 -0500

    standardized output and comments

--- src/com/gp/projecteuler/problems/Problem016.java
+++ src/com/gp/projecteuler/problems/Problem016.java
@@ -19,36 +19,18 @@ package com.gp.projecteuler.problems;
 
 import java.math.BigDecimal;
 
+/**
+	2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+	What is the sum of the digits of the number 2^1000?
+ */
 public class Problem016 {
 
-	/**
-	 * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-	 * What is the sum of the digits of the number 2^1000?
-	 * 
-	 * 
-	 * 2^1 = 2
-	 * 2^2 = 4
-	 * 2^3 = 8
-	 * 2^4 = 16 .. 7
-	 * 2^5 = 32 .. 5
-	 * 2^6 = 64 .. 10
-	 * 2^7 = 128 .. 11
-	 * 2^8 = 256 .. 13
-	 * 2^9 = 512 .. 8
-	 * 2^10 = 1024 .. 7
-	 * 2^11 = 2048 .. 14
-	 * 2^12 = 4096 .. 19
-	 * 2^13 = 8192 .. 20
-	 * 2^14 = 16384 .. 22
-	 * 2^15 = 32768 .. 26
-	 */
 	public static void main(String[] args) {
 		
 		BigDecimal bd = new BigDecimal(1);
 		
 		for(int i = 0; i < 1000; i++){
 			bd = bd.multiply(new BigDecimal(2));
-			System.out.println(bd.toString());
 		}
 		
 		int sum = 0;
@@ -56,7 +38,7 @@ public class Problem016 {
 			sum += Integer.valueOf(String.valueOf(c));
 		}
 		
-		System.out.println(sum);
+		System.out.println("Answer: " + sum);
 	}
 
 }
--- src/com/gp/projecteuler/problems/Problem018.java
+++ src/com/gp/projecteuler/problems/Problem018.java
@@ -22,55 +22,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.gp.projecteuler.FileUtil;
-import com.gp.projecteuler.Node;
 
 /**
- * By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+	By starting at the top of the triangle below and moving to adjacent numbers
+	on the row below, the maximum total from top to bottom is 23.
 
-		3
-		7 4
-		2 4 6
-		8 5 9 3
-		
-		That is, 3 + 7 + 4 + 9 = 23.
-		
-		Find the maximum total from top to bottom of the triangle below:
-		
-		75
-		95 64
-		17 47 82
-		18 35 87 10
-		20 04 82 47 65
-		19 01 23 75 03 34
-		88 02 77 73 07 63 67
-		99 65 04 28 06 16 70 92
-		41 41 26 56 83 40 80 70 33
-		41 48 72 33 47 32 37 16 94 29
-		53 71 44 65 25 43 91 52 97 51 14
-		70 11 33 28 77 73 17 78 39 68 17 57
-		91 71 52 38 17 14 91 43 58 50 27 29 48
-		63 66 04 68 89 53 67 30 73 16 69 87 40 31
-		04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
-
-		NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route.
- 		However, Problem 67, is the same challenge with a triangle containing one-hundred rows;
- 		it cannot be solved by brute force, and requires a clever method! ;o)
- * @author asus
- *
+	3
+	7 4
+	2 4 6
+	8 5 9 3
+	
+	That is, 3 + 7 + 4 + 9 = 23.
+	
+	Find the maximum total from top to bottom of the triangle below:
+	
+	75
+	95 64
+	17 47 82
+	18 35 87 10
+	20 04 82 47 65
+	19 01 23 75 03 34
+	88 02 77 73 07 63 67
+	99 65 04 28 06 16 70 92
+	41 41 26 56 83 40 80 70 33
+	41 48 72 33 47 32 37 16 94 29
+	53 71 44 65 25 43 91 52 97 51 14
+	70 11 33 28 77 73 17 78 39 68 17 57
+	91 71 52 38 17 14 91 43 58 50 27 29 48
+	63 66 04 68 89 53 67 30 73 16 69 87 40 31
+	04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+	
+	NOTE: As there are only 16384 routes, it is possible to solve this problem
+	by trying every route.	However, Problem 67, is the same challenge with a
+	triangle containing one-hundred rows; it cannot be solved by brute force,
+	and requires a clever method! ;o)
  */
 public class Problem018 {
 
 	/**
+	 * This solution is done by looking at the end of the triangle first and
+	 * then working backwards.  Each part in the tree has two children (except
+	 * for the last row).  The maximum weight of the parent can be determined
+	 * by choosing the larger of the two children.  On the next iteration that
+	 * parent becomes a child and then competes with its siblings for the
+	 * largest weight for its parent.  When the top of the triangle is reached
+	 * the maximum weight path is obtained.
+	 * 
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
 		
-		List<String> lines = FileUtil.getTextFromFile("./resources/Problem067.txt");
-//		Node root = getRootNode(lines);
-//		root.setTentativeWeight(0);
-//		
-//		Node currentNode = root;
+		// Problem 067 is identical to this problem but much larger.
+		// Change the input file to Problem067.txt to solve that problem
+		List<String> lines = FileUtil.getTextFromFile("./resources/Problem018.txt");
 		
 		List<List<Integer>> values = new ArrayList<List<Integer>>();
 		for(String s : lines){
@@ -83,6 +88,7 @@ public class Problem018 {
 			values.add(row);			
 		}
 		
+		// Print the original problem data
 		for(List<Integer> l : values){
 			System.out.println(l);
 		}
@@ -90,59 +96,27 @@ public class Problem018 {
 		System.out.println("###############################");
 		
 		for(int i = lines.size() - 1; i > 0; i--){
+			// Find the larger of the two child values and assign it and
+			// its parent's value to the parent.
 			for(int x = 0; x < values.get(i - 1).size(); x++){
-				int greaterValue = values.get(i).get(x) > values.get(i).get(x+1) ? values.get(i).get(x) : values.get(i).get(x+1);
+				int greaterValue = values.get(i).get(x) > values.get(i).get(x+1) ?
+						values.get(i).get(x) : values.get(i).get(x+1);
 				values.get(i - 1).set(x, values.get(i - 1).get(x) + greaterValue);
 			}
 			
+			// Set the children to zero
 			for(int x = 0; x < values.get(i).size(); x++){
 				values.get(i).set(x, 0);
 			}
 			
+			// Print this iteration's simplification of the triangle
 			for(List<Integer> l : values){
 				System.out.println(l);
 			}
 			
 			System.out.println("###############################");
 		}
-	}
-	
-	/*
-	public static Node getRootNode(List<String> lines){
-		Node root = null;
-		List<Node> previousRow = null;
 		
-		for(String line : lines){
-			if(root == null){
-				root = new Node(Long.valueOf(line));
-				previousRow = new ArrayList<Node>();
-				previousRow.add(root);
-			}
-			else{
-				String[] pieces = line.split(" ");
-				List<Node> rowNodes = new ArrayList<Node>();
-				
-				for(int i = 0; i < pieces.length; i++){
-					Node node = new Node(Long.valueOf(pieces[i]));
-					rowNodes.add(node);
-					
-					if(i == 0){
-						previousRow.get(0).getChildren().add(node);
-					}
-					else if(i == pieces.length - 1){
-						previousRow.get(i - 1).getChildren().add(node);
-					}
-					else{
-						previousRow.get(i - 1).getChildren().add(node);
-						previousRow.get(i).getChildren().add(node);
-					}
-				}
-				previousRow = rowNodes;
-			}			
-		}
-		
-		return root;
+		System.out.println("Answer: " + values.get(0).get(0));
 	}
-	*/
-
 }
