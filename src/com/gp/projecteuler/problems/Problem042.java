6165c3fa96904e1ddf4ccedfb018edfbdba783be
Gary Paduana <gary.paduana@gmail.com>
2013-05-28 16:14:46 -0500

    standardized output and comments

--- src/com/gp/projecteuler/problems/Problem042.java
+++ src/com/gp/projecteuler/problems/Problem042.java
@@ -26,15 +26,27 @@ import java.util.TreeSet;
 import com.gp.projecteuler.CommonMath;
 import com.gp.projecteuler.FileUtil;
 
+/**
+	The nth term of the sequence of triangle numbers is given by, 
+	t(n) = ½n(n+1); so the first ten triangle numbers are:
+	
+	1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+	
+	By converting each letter in a word to a number corresponding to its 
+	alphabetical position and adding these values we form a word value. 
+	For example, the word value for SKY is 19 + 11 + 25 = 55 = t(10). If the
+	word value is a triangle number then we shall call the word a triangle word.
+	
+	Using words.txt (right click and 'Save Link/Target As...'), a 16K text 
+	file containing nearly two-thousand common English words, how many are
+	triangle words?
+ */
 public class Problem042 {
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
 	public static void main(String[] args) throws IOException {
 				
-		List<String> words = Arrays.asList(FileUtil.getTextFromFile("./resources/Problem042.txt").get(0).split(","));
+		List<String> words = Arrays.asList(FileUtil.getTextFromFile(
+			"./resources/Problem042.txt").get(0).split(","));
 		
 		Set<Long> triangleSet = new TreeSet<Long>();
 		int count = 0;
@@ -46,13 +58,14 @@ public class Problem042 {
 		for(String word : words){
 			word = word.replace("\"", "");
 			
-			long wordValue = CommonMath.sumListOfNumber(CommonMath.charArrayToAlphaPositionList(word));
+			long wordValue = CommonMath.sumListOfNumber(
+				CommonMath.charArrayToAlphaPositionList(word));
 			
 			if(triangleSet.contains(wordValue)){
 				count++;
 			}
 		}
 		
-		System.out.println(count);
+		System.out.println("Answer: " + count);
 	}
 }
