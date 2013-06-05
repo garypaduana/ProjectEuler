6949191a5c83ca7cab8b1c4449bc0cca8cdef274
Gary Paduana <gary.paduana@gmail.com>
2013-06-05 16:20:23 -0500

    standardized output and comments
    exercises correct solution but needs improvement

--- src/com/gp/projecteuler/problems/Problem086.java
+++ src/com/gp/projecteuler/problems/Problem086.java
@@ -18,30 +18,46 @@
 package com.gp.projecteuler.problems;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
-import com.gp.projecteuler.FileUtil;
 import com.gp.projecteuler.UniqueCollection;
 
+/**
+	A spider, S, sits in one corner of a cuboid room, measuring 6 by 5 by 3,
+	and a fly, F, sits in the opposite corner. By travelling on the surfaces 
+	of the room the shortest "straight line" distance from S to F is 10 and 
+	the path is shown on the diagram.
+	
+		(please see image at: https://projecteuler.net/problem=86)
+		
+	However, there are up to three "shortest" path candidates for any given 
+	cuboid and the shortest route doesn't always have integer length.
+	
+	By considering all cuboid rooms with integer dimensions, up to a maximum 
+	size of M by M by M, there are exactly 2060 cuboids for which the shortest 
+	route has integer length when M=100, and this is the least value of M for 
+	which the number of solutions first exceeds two thousand; the number of 
+	solutions is 1975 when M=99.
+	
+	Find the least value of M such that the number of solutions first exceeds 
+	one million.
+ */
 public class Problem086 {
 
 	/**
+	 * TODO: Optimize this solution.  The following code verifies that the
+	 * answer is 1818 but it is not very efficient.
+	 * 
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
 		long start = System.currentTimeMillis();
 		
 		Map<UniqueCollection, Double> shortestIntegerPaths = new HashMap<UniqueCollection, Double>();
 		
-		for(int m = 1200; m <= 1200; m++){
+		for(int m = 1818; m <= 1818; m++){
 			for(int i = 1; i <= m; i++){
 				for(int j = 1; j <= m; j++){
 					for(int k = 1; k <= m; k++){
@@ -50,7 +66,8 @@ public class Problem086 {
 
 						if(shortestIntegerPaths.containsKey(uc)){
 							if(distance < shortestIntegerPaths.get(uc)){
-								shortestIntegerPaths.remove(uc);
+								System.out.println("removed: " + shortestIntegerPaths.remove(uc) + 
+									", " + shortestIntegerPaths.size());
 							}
 						}
 						
@@ -65,7 +82,7 @@ public class Problem086 {
 			shortestIntegerPaths.clear();
 		}
 		
-		System.out.println(System.currentTimeMillis() - start);
+		System.out.println("Duration: " + (System.currentTimeMillis() - start));
 	}
 	
 	public static double findMinimumPath(int width, int height, int depth){
