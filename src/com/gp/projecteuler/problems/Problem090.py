1c41bd656a12d982548fad49d7a700ce5c80f173
Gary Paduana <gary.paduana@gmail.com>
2013-06-19 00:06:05 -0500

    slow, incorrect solution

--- src/com/gp/projecteuler/problems/Problem090.py
+++ src/com/gp/projecteuler/problems/Problem090.py
@@ -22,58 +22,82 @@ def main():
     for i in range(0, 10):
         select_from_set.add(str(i))
 
+    needed = set()
+    [needed.add(x) for x in select_from_set]
+    needed.remove('7')
+
     print select_from_set
         
-    squares = [i**2 for i in range(1, 10)]
+    squares = [str(i**2).zfill(2) for i in range(1, 10)]
     print squares
-
+    last_count = -1
     valid_sets = set()
     
-    for perm in itertools.permutations(select_from_set, 6):
-        if('6' in perm and '9' not in perm):
-            perm = perm + tuple('9')
-        elif('9' in perm and '6' not in perm):
-            perm = perm + tuple('6')
-         
-        # for this permutation, find minimum matching set
-        other = set()
-        for square in squares:
-            found = True
+    for die1 in itertools.combinations(select_from_set, 6):
+        print die1
+        die2_needed = set(needed)
+        for num in die1:
+            die2_needed.discard(num)
+
+        #print "die1:", die1, "die2_needed:", die2_needed
+        how_many = 6 - len(die2_needed)
+
+        for die2_perm in itertools.combinations(select_from_set.difference(die2_needed), how_many):
+            #print "die2_perm:", die2_perm
+            m = makes_all_squares(set(die1), set(die2_perm).union(die2_needed), squares, False)
             
-            s = str(square)
-            if(square < 10):
-                s = "0" + str(square)
-                
-            if(s[0] in perm):
-                other.add(s[1])
-            elif(s[1] in perm):
-                other.add(s[0])
-            else:
-                found = False
-                break
+            if(m):
+                valid_sets.add(frozenset({frozenset(die1), frozenset(set(die2_perm).union(die2_needed))}))
+                if(last_count != len(valid_sets)):
+                    print len(valid_sets), die1, set(die2_perm).union(die2_needed)
+                    last_count = len(valid_sets)
 
-        if(len(other) > 6 or found == False):
-            continue
+def makes_all_squares(die1, die2, squares, swapped):
+    #"die1:", die1, "die2:", die2, "squares:", squares, "swapped:", swapped
+    found = True
+    for square in squares:
+        if not ((square[0] in die1 and square[1]  in die2) or
+                (square[0]  in die2 and square[1]  in die1)):
+            #print "cannot find:", square
+            found = False
 
-        # minimum set found, if 2nd die has fewer than 6 sides
-        # determined already, find all permutations to fill
-        # in the gaps.  These are rather meaningless and just filler.
+        if not found and not swapped:
+            if '6' in die1:
+                die1.add('9')
+            if '9' in die1:
+                die1.add('6')
+            if '6' in die2:
+                die2.add('9')
+            if '9' in die2:
+                die2.add('6')
+            return makes_all_squares(die1, die2, squares, True)
+    return found
 
-        difference = select_from_set.difference(other)
-
-        for diff_perm in itertools.permutations(difference, (6 - len(other))):
-            other_modified = set()
-            [other_modified.add(o) for o in other]
-                        
-            for additional_num in diff_perm:
-                other_modified.add(additional_num)
-                
-            #print "die 1:", perm, "die 2:", other_modified
-            valid_sets.add(frozenset({frozenset(perm), frozenset(other_modified)}))
-    print len(valid_sets)
-
-    for s in valid_sets:
-        print s
     
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
