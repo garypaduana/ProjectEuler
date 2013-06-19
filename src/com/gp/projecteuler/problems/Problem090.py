c21d7a33894b9bcc474a7aecf3c0b1db9063d7e9
Gary Paduana <gary.paduana@gmail.com>
2013-06-19 00:14:03 -0500

    correct solution
    used itertools.combinations instead of itertools.permutations.  this
    drastically reduced the number, n, in the n^2 complexity comparing all
    combinations of 6 selections from 10 items in both dies.

--- src/com/gp/projecteuler/problems/Problem090.py
+++ src/com/gp/projecteuler/problems/Problem090.py
@@ -16,49 +16,35 @@
 '''
 
 import itertools
+import datetime
 
 def main():
+    start = datetime.datetime.now()
     select_from_set = set()
     for i in range(0, 10):
         select_from_set.add(str(i))
-
-    needed = set()
-    [needed.add(x) for x in select_from_set]
-    needed.remove('7')
-
-    print select_from_set
         
     squares = [str(i**2).zfill(2) for i in range(1, 10)]
-    print squares
     last_count = -1
     valid_sets = set()
     
     for die1 in itertools.combinations(select_from_set, 6):
-        print die1
-        die2_needed = set(needed)
-        for num in die1:
-            die2_needed.discard(num)
-
-        #print "die1:", die1, "die2_needed:", die2_needed
-        how_many = 6 - len(die2_needed)
-
-        for die2_perm in itertools.combinations(select_from_set.difference(die2_needed), how_many):
-            #print "die2_perm:", die2_perm
-            m = makes_all_squares(set(die1), set(die2_perm).union(die2_needed), squares, False)
+        for die2 in itertools.combinations(select_from_set, 6):
+            m = makes_all_squares(set(die1), set(die2), squares, False)
             
             if(m):
-                valid_sets.add(frozenset({frozenset(die1), frozenset(set(die2_perm).union(die2_needed))}))
+                valid_sets.add(frozenset({frozenset(die1), frozenset(set(die2))}))
                 if(last_count != len(valid_sets)):
-                    print len(valid_sets), die1, set(die2_perm).union(die2_needed)
                     last_count = len(valid_sets)
+                    
+    print "Answer:", len(valid_sets)
+    print "Duration:", (datetime.datetime.now() - start)
 
 def makes_all_squares(die1, die2, squares, swapped):
-    #"die1:", die1, "die2:", die2, "squares:", squares, "swapped:", swapped
     found = True
     for square in squares:
         if not ((square[0] in die1 and square[1]  in die2) or
                 (square[0]  in die2 and square[1]  in die1)):
-            #print "cannot find:", square
             found = False
 
         if not found and not swapped:
@@ -73,7 +59,6 @@ def makes_all_squares(die1, die2, squares, swapped):
             return makes_all_squares(die1, die2, squares, True)
     return found
 
-    
 if __name__ == "__main__":
     main()
 
