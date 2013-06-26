3ea08112e8e4323be57e36b5b826c83ed1acb32c
Gary Paduana <gary.paduana@gmail.com>
2013-06-26 16:58:02 -0500

    correct solution using Heronian Triangles

--- src/com/gp/projecteuler/problems/Problem094.py
+++ src/com/gp/projecteuler/problems/Problem094.py
@@ -36,32 +36,48 @@ def main():
     start = datetime.datetime.now()
     sum = 0
     perimeter_sum = 0
-    side = 1
-    
-    for i in ranger(2, 1000000):
-        if(i % 10000000 == 0):
-            print i, "Duration:", (datetime.datetime.now() - start)
-        
-        a = Decimal(i+1)
-        b = Decimal(i)
-        p = Decimal((a*2+b)/Decimal(2.))
-        area1 = (p*(p-a)*(p-b)*(p-a))**(Decimal(1)/Decimal(2))
-        
-        a = i-1
-        b = i
-        p = (a*2+b)/2.
-        area2 = (p*(p-a)*(p-b)*(p-a))**(1./2.)
-        
-        if area1 == int(area1) and area1 != 0.0:
-            print (i+1),(i+1),i, area1
-            perimeter_sum += (i + 1) + (i + 1) + i
-        if area2 == int(area2) and area2 != 0.0:
-            print (i-1),(i-1),i, area2
-            perimeter_sum += (i - 1) + (i - 1) + i
 
-    print perimeter_sum
+    x_table = dict()
+    x_table[1] = (5,5,6,16)
+    x_table[2] = (65,65,66,196)
+    x_table[3] = (901,901,902,2704)
+    x_table[4] = (12545,12545,12546,37636)
+    
+    y_table = dict()
+    y_table[1] = (16,17,17,50)
+    y_table[2] = (240,241,241,722)
+    y_table[3] = (3360,3361,3361,10082)
+    y_table[4] = (46816,46817,46817,140450)
+
+    case = 5
+    while(x_table[len(x_table)][3] < 1000000000):
+        x_table[case] = find_case(case, x_table, "x_table")
+        case += 1
+
+    case = 5
+    while(y_table[len(y_table)][3] < 1000000000):
+        y_table[case] = find_case(case, y_table, "y_table")
+        case += 1
+
+    for key in x_table.iterkeys():
+        if(x_table[key][3] <= 1e9):
+            perimeter_sum += x_table[key][3]
+
+    for key in y_table.iterkeys():
+        if(y_table[key][3] <= 1e9):
+            perimeter_sum += y_table[key][3]
+
+    print "Answer:", perimeter_sum
     print "Duration:", (datetime.datetime.now() - start)
 
+def find_case(case, table, ab):
+    n = 15 * table[case-1][0] - 15 * table[case-2][0] + table[case-3][0]
+    if(ab == "x_table"):
+        tup = (n, n+1, n+1, (3*n+2))
+    elif(ab == "y_table"):
+        tup = (n, n, n+1, (3*n+1))
+    return tup   
+    
 def ranger(lower, upper):
     num = lower - 1
     while num < (upper - 1):
