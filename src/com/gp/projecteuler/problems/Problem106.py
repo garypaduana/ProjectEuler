cfe517a1ea56432d067852c76fc6a020eae97a7b
Gary Paduana <gary.paduana@gmail.com>
2014-03-19 11:46:56 -0400

    rearranged the imports to include CommonPy.  the way it was previously defined caused an exception.

--- src/com/gp/projecteuler/problems/Problem106.py
+++ src/com/gp/projecteuler/problems/Problem106.py
@@ -15,11 +15,11 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+import sys
+sys.path.append('../')
 import CommonPy
 import datetime
 import itertools
-import sys
-sys.path.append('../')
 
 class Pair:    
     def __init__(self, b, c):
